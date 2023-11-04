a7f2ab9197bfd61541c06d439a6098bcd0a8a39d
thatnoobles <danildonchuk@gmail.com>
2023-11-04 15:55:05 -0400

    PR edit

--- adc-emulation/raspi/receiver.py
+++ adc-emulation/raspi/receiver.py
@@ -22,6 +22,7 @@ def get_value_from_adc(channel : int) -> int:
             try:
                 result = bus.read_word_data(I2C_ADDR, command_byte)
             except Exception as e:
+                print("Failed to read, trying again...")
                 continue  # just try again
             break
 
