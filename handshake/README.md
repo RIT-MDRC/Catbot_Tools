86a07e6c3594e83fa34e6a37c7bc40bf15244c33
thatnoobles <danildonchuk@gmail.com>
2023-11-05 12:12:41 -0500

    updated to be threshold instead of range

--- handshake/README.md
+++ handshake/README.md
@@ -1,10 +1,10 @@
 # Features
-Arduino sends a "handshake" signal to Raspberry Pi so it knows if the pneumatics system is operating within a decent range. Sets the signal HIGH if the pressure is OK; LOW otherwise.
+Arduino sends a "handshake" signal to Raspberry Pi so it knows if the pneumatics system is operating at sufficient pressure. Sets the signal HIGH if the pressure is OK; LOW otherwise.
 
 # Usage
 Connect a jumper wire from the Arduino pin defined in its code (`handshake/arduino/arduino.ino`) to Raspberry Pi's defined pin (`handshake/raspi/receive_handshake.py`). Upload the Arduino code (and set `ENABLE_MOCKING` based on if you have real hardware), and run `handshake/raspi/main.py` to run the sample.
 
-**Note:** If you are using an Arduino that operates at anything higher than 5V, you'll need to have a logic level converter set up between it and the Pi.
+**NOTE:** If you are using an Arduino that operates at anything higher than 5V, you'll need to have a logic level converter set up between it and the Pi.
 
 # Development
 ## Date updated: 11/05/2023
--- handshake/arduino/arduino.ino
+++ handshake/arduino/arduino.ino
@@ -1,19 +1,18 @@
 /*
  * HANDSHAKE
  * Sends a binary signal (0 or 1) to the Raspberry Pi, representing whether or not
- * the current pneumatics pressure is within operating range.
+ * the current pneumatics pressure is sufficient for leg movements.
 */
 
-// The range defining what pressure level is ok to operate at (in psi)
-#define MIN_OPERATING_PRESSURE 65
-#define MAX_OPERATING_PRESSURE 80
+// Pressure level that we need to hit to be able to flex the leg (in psi)
+#define SUFFICIENT_PRESSURE 80
 
 // Set to false if testing on real hardware
 #define ENABLE_MOCKING true
 
 // Mock bounds
-#define MOCK_MIN 50
-#define MOCK_MAX 100
+#define MOCK_MIN 0
+#define MOCK_MAX 145
 bool increasing = true;  // Once we reach MOCK_MAX, decrease until we hit MOCK_MIN
 
 // The pin that is sending data to the Pi
@@ -24,6 +23,7 @@ int currentPressure = 0;  // (in psi)
 void setup()
 {
   pinMode(OUTPUT_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   if (ENABLE_MOCKING)
     currentPressure = MOCK_MIN;
@@ -32,7 +32,7 @@ void setup()
 void loop()
 {
   // If using test data, oscillate the "pressure reading" between a defined min/max. Preferably this
-  // contains the operating range.
+  // contains the sufficient pressure level.
   if (ENABLE_MOCKING)
   {
     currentPressure += increasing ? 1 : -1;
@@ -49,8 +49,9 @@ void loop()
     // gotten to try this with the actual pneumatics system yet.
   }
 
-  bool pressureInBounds = MIN_OPERATING_PRESSURE <= currentPressure && currentPressure <= MAX_OPERATING_PRESSURE;
-  digitalWrite(OUTPUT_PIN, pressureInBounds);
+  bool atSufficientPressure = currentPressure >= SUFFICIENT_PRESSURE;
+  digitalWrite(OUTPUT_PIN, atSufficientPressure);
+  digitalWrite(LED_BUILTIN, atSufficientPressure);  // turn on LED at sufficient pressure for debugging
 
   delay(10);
 }
