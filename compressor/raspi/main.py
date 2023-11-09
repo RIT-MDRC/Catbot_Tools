7151792667414c947057679128cf199befb27cb0
thatnoobles <danildonchuk@gmail.com>
2023-11-09 18:35:52 -0500

    comments <3

--- compressor/raspi/main.py
+++ compressor/raspi/main.py
@@ -1,6 +1,9 @@
+# COMPRESSOR
+# A simple tool to turn the air compressor on/off.
+
 import RPi.GPIO as GPIO
 
-OUTPUT_PIN = 21
+OUTPUT_PIN = 21  # GPIO 21, not board pin 21
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(OUTPUT_PIN, GPIO.OUT)
@@ -12,8 +15,10 @@ try:
     while True:
         input(f"Press enter to toggle compressor {'on' if set_high == 1 else 'off'}")
 
+        # Toggle what pressing enter will do next time
         GPIO.output(OUTPUT_PIN, set_high == 1)
         set_high = 1 if set_high == 0 else 0
+
 except KeyboardInterrupt:
     GPIO.cleanup()
     print("\nEnded")
